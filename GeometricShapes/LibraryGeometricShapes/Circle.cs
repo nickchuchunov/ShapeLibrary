bebc2139b007b7793047b8261fd30eea720707a2
nick.chuchunov <nick.chuchunov@yandex.ru>
2022-07-02 10:41:38 +0300

    создание классов треугольник и круг

---
+++ GeometricShapes/LibraryGeometricShapes/Circle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryGeometricShapes
+{
+    internal class Circle
+    {
+        double Square(double r) 
+        { 
+         double s= 2*Math.PI*Math.Pow(r,2);
+            return s;
+        }
+
+
+    }
+}
--- GeometricShapes/LibraryGeometricShapes/Triangle.cs
+++ GeometricShapes/LibraryGeometricShapes/Triangle.cs
@@ -2,8 +2,12 @@
 {
     public class Triangle
     {
-
-
+        double Square(double a, double b, double c)
+        {
+            double p = a + b + c / 2;
+            double s = Math.Sqrt(p * (p- a) * (p- b) * (p-c));
+            return s;
+        }
 
     }
 }
